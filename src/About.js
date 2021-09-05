5525ccd815963387ccca999cf0a37276e7d49225
Nick Schomaker <87150577+nick-nsc@users.noreply.github.com>
2021-09-05 17:00:42 +0200

    React app with HashRouter to publish to Github Pages

---
+++ src/About.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>About Page</h2>
+                <main>
+                    <p>This section contains information about...</p>
+                </main>
+            </div>
+        );
+    }
+}     
+
+export default About;
