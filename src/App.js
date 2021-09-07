15d203c9bbef0428bfa8b86dc7af377980625cf1
Nick Schomaker <87150577+nick-nsc@users.noreply.github.com>
2021-09-07 14:53:21 +0200

    added p5.js example project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 class App extends React.Component {
   render(){
@@ -18,7 +19,8 @@ class App extends React.Component {
 
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/about" component={About} />          
         </Switch>
       </div>
     );
---
+++ src/pages/Projects.js
@@ -0,0 +1,105 @@
+import './../App.css';
+import React from 'react';
+import p5 from 'p5';
+
+class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+        this.myRef = React.createRef();
+    }
+
+    //p5 instance mode to create a sketch, p is a p5 object
+    Sketch = (p) => {
+                
+        /*
+        * @name Spirograph
+        * @description This sketch uses simple transformations to create a
+        * Spirograph-like effect with interlocking circles (called sines).
+        * Press the spacebar to switch between tracing and showing the underlying geometry.<br>
+        * Example created by <a href='http://lukedubois.com/' target='_blank'>R. Luke DuBois</a>.<br>
+        * <a href='http://en.wikipedia.org/wiki/Spirograph'>http://en.wikipedia.org/wiki/Spirograph</a>
+        */
+        
+        let NUMSINES = 5; // how many of these things can we do at once?
+        let sines = new Array(NUMSINES); // an array to hold all the current angles
+        let rad; // an initial radius value for the central sine
+
+        // play with these to get a sense of what's going on:
+        let fund = 0.005; // the speed of the central sine
+        let ratio = 1; // what multiplier for speed is each additional sine?
+        let alpha = 50; // how opaque is the tracing system
+
+        let trace = true; // are we tracing?
+
+        //setup the canvas
+        p.setup = () => {
+            p.createCanvas(710, 400);
+
+            rad = p.height / 4; // compute radius for central circle
+            p.background(204); // clear the screen
+
+            for (let i = 0; i<sines.length; i++) {
+                sines[i] = p.PI; // start EVERYBODY facing NORTH
+            }
+        }
+
+        //drawing stuff
+        p.draw = () => {
+            if (!trace) {
+                p.background(204); // clear screen if showing geometry
+                p.stroke(0, 255); // black pen
+                p.noFill(); // don't fill
+            }
+            
+            // MAIN ACTION
+            p.push(); // start a transformation matrix
+            p.translate(p.width / 2, p.height / 2); // move to middle of screen
+        
+            for (let i = 0; i < sines.length; i++) {
+            let erad = 0; // radius for small "point" within circle... this is the 'pen' when tracing
+            // setup for tracing
+            if (trace) {
+                p.stroke(0, 0, 255 * (p.float(i) / sines.length), alpha); // blue
+                p.fill(0, 0, 255, alpha / 2); // also, um, blue
+                erad = 5.0 * (1.0 - p.float(i) / sines.length); // pen width will be related to which sine
+            }
+            let radius = rad / (i + 1); // radius for circle itself
+            p.rotate(sines[i]); // rotate circle
+            if (!trace) p.ellipse(0, 0, radius * 2, radius * 2); // if we're simulating, draw the sine
+            p.push(); // go up one level
+            p.translate(0, radius); // move to sine edge
+            if (!trace) p.ellipse(0, 0, 5, 5); // draw a little circle
+            if (trace) p.ellipse(0, 0, erad, erad); // draw with erad if tracing
+            p.pop(); // go down one level
+            p.translate(0, radius); // move into position for next sine
+            sines[i] = (sines[i] + (fund + (fund * i * ratio))) % p.TWO_PI; // update angle based on fundamental
+            }
+        
+            p.pop(); // pop down final transformation
+        }
+    }
+
+    //after rendering, create the p5 instance and attach it to the given DOM node
+    componentDidMount() {
+        this.myP5 = new p5(this.Sketch, this.myRef.current);
+    }
+
+    render() {
+        return (
+            <div name="projects-frame">
+                <div>
+                    <h1>Example p5.js sketch</h1>
+                </div>
+                <div ref={this.myRef} style={{ 'margin-top': '50px' }}>
+
+                </div>
+                <div>
+                    Credits: Created by <a href='http://lukedubois.com/' target='_blank' rel='noreferrer'>R. Luke DuBois</a>.
+                </div>
+            </div>
+            
+        );
+    }
+}     
+
+export default Projects;
